5420038a0bf05129f470c44d21eb587d3a540dac
marijusiMac <marijuslau@yahoo.com>
2019-08-02 13:39:42 +0300

    Get grouped schedule

--- iOS-Entry-Marijus-Laucevicius.playground/Contents.swift
+++ iOS-Entry-Marijus-Laucevicius.playground/Contents.swift
@@ -21,9 +21,8 @@ struct Shop {
     }
 }
 
-//
 func printSchedule(shop: Shop) {
-    var schedule : [String : String] = ["I" : "Closed"]
+    var schedule = [String : String]()
     
     schedule["I"] = checkHrs(workHrs: shop.workSchedule.monday)
     schedule["II"] = checkHrs(workHrs: shop.workSchedule.tuesday)
@@ -33,11 +32,11 @@ func printSchedule(shop: Shop) {
     schedule["VI"] = checkHrs(workHrs: shop.workSchedule.saturday)
     schedule["VII"] = checkHrs(workHrs: shop.workSchedule.sunday)
     
-    let sortedKeys = Array(schedule.keys).sorted(by: <)
-
-    print(schedule)
     
-    print(sortedKeys)
+    let sortedKeys = Array(schedule.keys).sorted(by: <)
+    
+    print("------------" + shop.name + " schedule: ")
+    print(groupSchedule(schedule: schedule, keys: sortedKeys))
 }
 
 func checkHrs(workHrs : Shop.WorkHours?) -> String {
@@ -48,12 +47,76 @@ func checkHrs(workHrs : Shop.WorkHours?) -> String {
     }
 }
 
+func groupSchedule(schedule: [String : String], keys: Array<String>) -> [String : String] {
+    var groupedSchedule = [String : String]()
+    
+    var sortedDays = [" ",]
+    
+    var nextKeys = keys
+    
+    var temp : String? = nil
+    
+    for day in keys {
+        nextKeys.removeFirst(1)
+        if !(sortedDays.contains(day)) {
+            
+            for nextDay in nextKeys {
+                if schedule[day] == schedule[nextDay] {
+                    sortedDays.append(nextDay)
+                    if let lastDay = temp{
+                        groupedSchedule.removeValue(forKey: lastDay)
+                        groupedSchedule[day + "-" + nextDay] = schedule[day]
+                        temp = day + "-" + nextDay
+                    } else {
+                        groupedSchedule[day + "-" + nextDay] = schedule[day]
+                        temp = day + "-" + nextDay
+                    }
+                } else {
+                    groupedSchedule[nextDay] = schedule[nextDay]
+                    temp = nextDay
+                    break
+                }
+            }
+        }
+        
+    }
+    
+    return groupedSchedule
+}
+
 
 
 
 
 // Test data
-let shopOne = Shop(name: "Maxima", adress: "Ukmergės g. 264", workSchedule: Shop.WorkSchedule(monday: nil, tuesday: Shop.WorkHours(from: "10:00", to: "16:00"), wednesday: Shop.WorkHours(from: "10:00", to: "16:00"), thursday: Shop.WorkHours(from: "10:00", to: "16:00"), friday: Shop.WorkHours(from: "10:00", to: "14:00"), saturday: nil, sunday: nil))
+let shopOne = Shop(name: "Maxima", adress: "Ukmergės g. 264", workSchedule: Shop.WorkSchedule(
+    monday: Shop.WorkHours(from: "10:00", to: "16:00"),
+    tuesday: Shop.WorkHours(from: "10:00", to: "16:00"),
+    wednesday: Shop.WorkHours(from: "10:00", to: "16:00"),
+    thursday: Shop.WorkHours(from: "10:00", to: "16:00"),
+    friday: Shop.WorkHours(from: "10:00", to: "14:00"),
+    saturday: nil,
+    sunday: nil))
+
+let shopTwo = Shop(name: "Iki", adress: "Santariškių g. 1", workSchedule: Shop.WorkSchedule(
+    monday: Shop.WorkHours(from: "8:00", to: "22:00"),
+    tuesday: Shop.WorkHours(from: "8:00", to: "22:00"),
+    wednesday: Shop.WorkHours(from: "8:00", to: "22:00"),
+    thursday: Shop.WorkHours(from: "8:00", to: "22:00"),
+    friday: Shop.WorkHours(from: "8:00", to: "22:00"),
+    saturday: Shop.WorkHours(from: "8:00", to: "22:00"),
+    sunday: nil))
+
+let shopThree = Shop(name: "Rimi", adress: "Ukmergės g. 10", workSchedule: Shop.WorkSchedule(
+    monday: nil,
+    tuesday: nil,
+    wednesday: Shop.WorkHours(from: "10:00", to: "15:00"),
+    thursday: Shop.WorkHours(from: "10:00", to: "16:00"),
+    friday: Shop.WorkHours(from: "10:00", to: "14:00"),
+    saturday: nil,
+    sunday: nil))
 
 // Test the app
 printSchedule(shop: shopOne)
+printSchedule(shop: shopTwo)
+printSchedule(shop: shopThree)
